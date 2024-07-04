6499c68219d971a561505742f52272e2e1538d18
222112038 <falanarofako29@gmail.com>
2024-07-04 16:09:22 +0700

    set vercel config again

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,52 +1,68 @@
-const User = require('../models/User');
-const jwt = require('jsonwebtoken');
+const User = require("../models/User");
+const jwt = require("jsonwebtoken");
 
 const generateToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET, {
-    expiresIn: '30d',
-  });
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
+        expiresIn: "30d",
+    });
 };
 
 exports.signup = async (req, res) => {
-  const { username, email, password } = req.body;
+    const { username, email, password } = req.body;
 
-  try {
-    const userExists = await User.findOne({ email });
+    try {
+        const userExists = await User.findOne({ email });
 
-    if (userExists) {
-      return res.status(400).json({ message: 'User already exists' });
+        if (userExists) {
+            return res.status(400).json({
+                statusCode: 400, message: "User already exists"
+            });
+        }
+
+        const user = await User.create({ username, email, password });
+
+        res.status(201).json({
+            statusCode: 201,
+            message: "User created successfully",
+            data: {
+                _id: user._id,
+                username: user.username,
+                email: user.email,
+                token: generateToken(user._id),
+            }
+        });
+    } catch (error) {
+        res.status(500).json({
+            statusCode: 500, message: "Server error"
+        });
     }
-
-    const user = await User.create({ username, email, password });
-
-    res.status(201).json({
-      _id: user._id,
-      username: user.username,
-      email: user.email,
-      token: generateToken(user._id),
-    });
-  } catch (error) {
-    res.status(500).json({ message: 'Server error' });
-  }
 };
 
 exports.signin = async (req, res) => {
-  const { email, password } = req.body;
+    const { email, password } = req.body;
 
-  try {
-    const user = await User.findOne({ email });
+    try {
+        const user = await User.findOne({ email });
 
-    if (user && (await user.matchPassword(password))) {
-      res.json({
-        _id: user._id,
-        username: user.username,
-        email: user.email,
-        token: generateToken(user._id),
-      });
-    } else {
-      res.status(401).json({ message: 'Invalid email or password' });
+        if (user && (await user.matchPassword(password))) {
+            res.json({
+                statusCode: 200,
+                message: "Logged in successfully",
+                data: {
+                    _id: user._id,
+                    username: user.username,
+                    email: user.email,
+                    token: generateToken(user._id),
+                }
+            });
+        } else {
+            res.status(401).json({
+                statusCode: 401, message: "Invalid email or password"
+            });
+        }
+    } catch (error) {
+        res.status(500).json({
+            statusCode: 500, message: "Server error"
+        });
     }
-  } catch (error) {
-    res.status(500).json({ message: 'Server error' });
-  }
 };
